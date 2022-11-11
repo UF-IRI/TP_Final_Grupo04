9dc3fef333884c1511374249238fa6fe11ba99dc
Mariipp <mariapereyra265@gmail.com>
2022-11-11 20:41:44 -0300

    comentarios

--- libreria/funciones.h
+++ libreria/funciones.h
@@ -54,7 +54,20 @@ typedef struct Paciente {
 	string Estado; //son 3: n/c,fallecio, internado
 	string Obra_soc;
 };
+/// <summary>
+/// funciones de archivos
+/// </summary>
+/// <param name="nombredearchivo"></param>
+/// <param name="Lista_pacientes"></param>
+/// <param name="tam"></param>
+/// <returns></returns>
 bool leer_pacientes(string nombredearchivo,Paciente*& Lista_pacientes, int* tam );//esta funcion lee archivo pacientes.csv y los guarda en lista
+
+/// <summary>
+/// funciones que  chequean datos
+/// </summary>
+/// <param name="dni"></param>
+/// <returns></returns>
 bool chequear_DNI(string dni);
 bool chequeo_AyN(string& AyN); // esta funcion chequea nombre y apellidos de pacientes
 bool chequeo_telefono(string tel);
@@ -66,6 +79,8 @@ bool chequeargenero(char letra);
 bool chequearfecha_nacimiento(string fecha); // rechequear 
 bool chequeoObra_social(string Obra_soc, string *listaObra_soc, int tam);//mira lista de obras sociales creada a partir de obrassociales.csv
 bool chequeo_especialedad(string e);
+
+///funciones de listas
 bool agregar_paciente(Paciente aux, Paciente*& lista, int* tam);
 int buscarpaciente(string dni, Paciente* lista, int tam);//busca al paciente en la lista si lo encuentra devuelv pos,-1 si no esta o error
 bool redimensionar(Paciente*& lista, int* tam, int cant_aumentar);//redimensiona una lista de pacientes
