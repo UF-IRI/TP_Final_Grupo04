2c6ea1f8ab4bad14fd4b5c8a36f0d04f67b8a814
Sofia Ogando <84402232+Sofia-Ogando18@users.noreply.github.com>
2022-11-18 00:04:42 -0300

    ultimos toques

--- main/main.cpp
+++ main/main.cpp
@@ -15,20 +15,21 @@ int main() {
     Paciente* Lista_Pacientes = new Paciente[0];
     Medicos* Lista_Medicos = new Medicos[0];
     Contactos* Lista_Contactos = new Contactos[0];
-    Contactos* sub_lista = new Contactos[0];
+    Paciente* sub_lista = new Paciente[0];
     
    leer_medicos("C:/Users/Electro PC/Desktop/facultad/Redes e Internet/Final 2022/final/data_files/input/Medicos.csv",Lista_Medicos,&tam_medico);
    leer_Consultas("C:/Users/Electro PC/Desktop/facultad/Redes e Internet/Final 2022/final/data_files/input/Consultas.csv", Lista_Consultas, &tam_consulta);
    leer_pacientes("C:/Users/Electro PC/Desktop/facultad/Redes e Internet/Final 2022/final/data_files/input/Pacientes.csv",Lista_Pacientes,&tam_paciente);
    Leer_Contacto("C:/Users/Electro PC/Desktop/facultad/Redes e Internet/Final 2022/final/data_files/input/Contactos.csv",Lista_Contactos,&tam_contacto);
   
-   chequeo_10_anios(Lista_Pacientes,tam_paciente,Lista_Consultas,tam_consulta,)
-    //leer_Consultas("")
+   sub_lista=chequeo_10_anios(Lista_Pacientes, tam_paciente, Lista_Consultas, tam_consulta,&tam_sublista );
+   
     
 
     delete[] Lista_Consultas;
     delete[]  Lista_Contactos;
     delete[] Lista_Medicos;   
     delete[] Lista_Pacientes;
+    delete[] sub_lista;
     return EXIT_SUCCESS;
 }
