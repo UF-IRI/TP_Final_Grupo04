da7eec08d2345040d6980eec72c32cbc20b08364
Mariipp <mariapereyra265@gmail.com>
2022-11-25 16:29:01 -0300

    cambios en unitest

--- unit-test/casos_base.cpp
+++ unit-test/casos_base.cpp
@@ -1,19 +1,19 @@
 #include "gmock/gmock.h"
 #include"funciones.h"
-//namespace Casos_Base::tests {
-//	TEST(Casos_Base, Test_Caso1) {
-//		ASSERT_THAT(1, 1);
-//	}
-//
-//	TEST(Casos_Base, Test_Caso2) {
-//		ASSERT_THAT(1, 1);
-//	}
-//
-//	TEST(Casos_Base, Test_Caso3) {
-//		ASSERT_THAT(1, 1);
-//	}
-//}
-////bool redimensionarp(Paciente*& lista, int* tam, int cant_aumentar);
+namespace Casos_Base::tests {
+	TEST(Casos_Base, Test_Caso1) {
+		ASSERT_THAT(1, 1);
+	}
+
+	TEST(Casos_Base, Test_Caso2) {
+		ASSERT_THAT(1, 1);
+	}
+	TEST(Casos_Base, Test_Caso3) {
+		ASSERT_THAT(1, 1);
+	}
+
+}
+
 //TEST(redimensionarp, redimensionarp_CORRECTO)
 //{
 //	Paciente* lista = new Paciente[1];
@@ -35,11 +35,6 @@
 //	ASSERT_FALSE(redimensionarp(lista, &tam, cant_aumentar));
 //	delete[] lista;
 //}
-//
-////bool redimensionar_medicos(Medicos*& lista, int cantidad_aumentar, int* tam);
-////bool Redimensionar_Consultas(Consulta*& list, int tam);
-//
-//
 //TEST(redimensionar_medicos, redimensionar_medicos_CORRECTO)
 //{
 //	Medicos* lista = new Medicos[1];
@@ -82,167 +77,165 @@
 //	ASSERT_FALSE(Redimensionar_Consultas(lista, tam));
 //	delete[] lista;
 //}
-//
-//
+
 ////chequeo dni
-//TEST(chequear_DNI,DNI_letras)
-//{
-//	ASSERT_FALSE(chequear_DNI("4395815pl"));
-//}
-//TEST(chequear_DNI, DNI_simbolos)
-//{
-//	ASSERT_FALSE(chequear_DNI("4395/81%"));
-//}
-//TEST(chequear_DNI, DNI_espacios)
-//{
-//	ASSERT_FALSE(chequear_DNI("4395 81%"));
-//}
-//TEST(chequear_DNI, DNI_comun)
-//{
-//	ASSERT_TRUE(chequear_DNI("439581234"));
-//}
+TEST(chequear_DNI,DNI_letras)
+{
+	ASSERT_FALSE(chequear_DNI("4395815l"));
+}
+TEST(chequear_DNI, DNI_simbolos)
+{
+	ASSERT_FALSE(chequear_DNI("4395/81%"));
+}
+TEST(chequear_DNI, DNI_espacios)
+{
+	ASSERT_FALSE(chequear_DNI("4395 822"));
+}
+TEST(chequear_DNI, DNI_comun)
+{
+	ASSERT_TRUE(chequear_DNI("43958124"));
+}
 ////bool chequeo_AyN(string AyN)
-//TEST(chequeo_AyN,nombre_numeros)
-//{
-//	ASSERT_FALSE(chequeo_AyN("marian3o"));
-//}
-//TEST(chequeo_AyN, nombre_numeros_esp)
-//{
-//	ASSERT_FALSE(chequeo_AyN("marianno 100"));
-//}
-//TEST(chequeo_AyN, nombre_simbolos)
-//{
-//	ASSERT_FALSE(chequeo_AyN("mari%ano !ariel"));
-//}
-//
-//TEST(chequeo_AyN, nombre_comun_esp)
-//{
-//	ASSERT_TRUE(chequeo_AyN("mariano carlos"));
-//}
-//TEST(chequeo_AyN, nombre_comun_mayus)
-//{
-//	ASSERT_TRUE(chequeo_AyN("Mariano Carlos"));
-//}
+TEST(chequeo_AyN,nombre_numeros)
+{
+	ASSERT_FALSE(chequeo_AyN("marian3o"));
+}
+TEST(chequeo_AyN, nombre_numeros_esp)
+{
+	ASSERT_FALSE(chequeo_AyN("marianno 100"));
+}
+TEST(chequeo_AyN, nombre_simbolos)
+{
+	ASSERT_FALSE(chequeo_AyN("mari%ano !ariel"));
+}
+
+TEST(chequeo_AyN, nombre_comun_esp)
+{
+	ASSERT_TRUE(chequeo_AyN("mariano carlos"));
+}
+TEST(chequeo_AyN, nombre_comun_mayus)
+{
+	ASSERT_TRUE(chequeo_AyN("Mariano Carlos"));
+}
 //////bool chequeo_Mail(string mail);
-//TEST(chequeo_Mail, mail_sin_punto)
-//{
-//	ASSERT_FALSE(chequeo_Mail("favaloroalumnos@gmailcom"));
-//}
-//TEST(chequeo_Mail, mail_sin_arroba)
-//{
-//	ASSERT_FALSE(chequeo_Mail("favaloroalumnosgmail.com"));
-//}
-//TEST(chequeo_Mail, mail_espacios)
-//{
-//	ASSERT_FALSE(chequeo_Mail("favaloroalumnos @gmail.com"));
-//}
-//TEST(chequeo_Mail, mail_simbolos)
-//{
-//	ASSERT_FALSE(chequeo_Mail("faval%oroa!lumno@gmail.com"));
-//}
-//TEST(chequeo_Mail, mail_comun1)
-//{
-//	ASSERT_TRUE(chequeo_Mail("favaloroalumnos@gmail.com"));
-//}
-//TEST(chequeo_Mail, mail_comun2)
-//{
-//	ASSERT_TRUE(chequeo_Mail("mariapereyra265@gmail.com"));
-//}
-//TEST(chequeo_Mail, mail_comun3)
-//{
-//	ASSERT_TRUE(chequeo_Mail("uf@gmail.com.ar"));
-//}
+TEST(chequeo_Mail, mail_sin_punto)
+{	ASSERT_FALSE(chequeo_Mail("favaloroalumnos@gmailcom"));
+
+}
+TEST(chequeo_Mail, mail_sin_arroba)
+{
+	ASSERT_FALSE(chequeo_Mail("favaloroalumnosgmail.com"));
+}
+TEST(chequeo_Mail, mail_espacios)
+{
+	ASSERT_FALSE(chequeo_Mail("favaloroalumnos @gmail.com"));
+}
+TEST(chequeo_Mail, mail_simbolos)
+{
+	ASSERT_FALSE(chequeo_Mail("faval%oroa!lumno@gmail.com"));
+}
+TEST(chequeo_Mail, mail_comun1)
+{
+	ASSERT_TRUE(chequeo_Mail("favaloroalumnos@gmail.com"));
+}
+TEST(chequeo_Mail, mail_comun2)
+{
+	ASSERT_TRUE(chequeo_Mail("mariapereyra265@gmail.com"));
+}
+TEST(chequeo_Mail, mail_comun3)
+{
+	ASSERT_TRUE(chequeo_Mail("uf@gmail.com.ar"));
+}
 ////bool chequeargenero(char letra);
-//TEST(chequeargenero, genero_M)
-//{
-//	ASSERT_TRUE(chequeargenero('M'));
-//}
-//TEST(chequeargenero, genero_F)
-//{
-//	ASSERT_TRUE(chequeargenero('F'));
-//}
-//TEST(chequeargenero, genero_f)
-//{
-//	ASSERT_TRUE(chequeargenero('f'));
-//}
-//TEST(chequeargenero, genero_x)
-//{
-//	ASSERT_FALSE(chequeargenero('x'));
-//}
-//TEST(chequeargenero, genero_simbolos)
-//{
-//	ASSERT_FALSE(chequeargenero('%'));
-//}
-//TEST(chequeargenero, genero_numeros)
-//{
-//	ASSERT_FALSE(chequeargenero('2'));
-//}
+TEST(chequeargenero, genero_M)
+{
+	ASSERT_TRUE(chequeargenero('M'));
+}
+TEST(chequeargenero, genero_F)
+{
+	ASSERT_TRUE(chequeargenero('F'));
+}
+TEST(chequeargenero, genero_f)
+{
+	ASSERT_TRUE(chequeargenero('f'));
+}
+TEST(chequeargenero, genero_x)
+{
+	ASSERT_FALSE(chequeargenero('x'));
+}
+TEST(chequeargenero, genero_simbolos)
+{
+	ASSERT_FALSE(chequeargenero('%'));
+}
+TEST(chequeargenero, genero_numeros)
+{
+	ASSERT_FALSE(chequeargenero('2'));
+}
 ////bool chequeo_direccion(string direc);
-//TEST(chequeo_direccion,direc_simbolos)
-//{
-//	ASSERT_FALSE(chequeo_direccion("centr!o a(sturiano"));
-//}
-//TEST(chequeo_direccion, direc_may)
-//{
-//	ASSERT_TRUE(chequeo_direccion("Centro Asturiano"));
-//}
-//TEST(chequeo_direccion, direc_puntos)
-//{
-//	ASSERT_FALSE(chequeo_direccion("centro asturian.o"));
-//}
-//TEST(chequeo_direccion, direc_comun1)
-//{
-//	ASSERT_TRUE(chequeo_direccion("centro asturiano"));
-//}
-//TEST(chequeo_direccion, direc_comun2)
-//{
-//	ASSERT_TRUE(chequeo_direccion("centro asturiano 1234"));
-//}
-//TEST(chequeo_celular, cel_simbolos)
-//{
-//	ASSERT_FALSE(chequeo_celular("2*$,55"));
-//}
-//TEST(chequeo_celular, cel_letras)
-//{
-//	ASSERT_FALSE(chequeo_celular("2abbbbc"));
-//}
-//TEST(chequeo_celular, cel_comun)
-//{
-//	ASSERT_TRUE(chequeo_celular("+57 722 907 7201"));
-//}
-//TEST(chequearfecha, fecha_negativa )
-//{
-//	ASSERT_FALSE(chequearfecha(-2, -6, -2020));
-//}
-//TEST(chequearfecha, fecha_larga)
-//{
-//	ASSERT_FALSE(chequearfecha(222, 500, 22020));
-//}
-//TEST(chequearfecha, fecha_mesmal)
-//{
-//	ASSERT_FALSE(chequearfecha(2, 16, 2020));
-//}
-//TEST(chequearfecha, fecha_comun)
-//{
-//	ASSERT_TRUE(chequearfecha(15, 11, 2022));
-//}
-//TEST(chequeo_especialidades,especialidad_numero )
-//{
-//	ASSERT_FALSE(chequeo_especialidades("55555"));
-//}
-//TEST(chequeo_especialidades, especialidad_simbolos)
-//{
-//	ASSERT_FALSE(chequeo_especialidades("¿?-¨´"));
-//}
-//TEST(chequeo_especialidades, especialidad_mayuscula)
-//{
-//	ASSERT_TRUE(chequeo_especialidades("medico Clinico"));
-//}
-//TEST(chequeo_especialidades, especialidad_minuscula)
-//{
-//	ASSERT_TRUE(chequeo_especialidades("medico pediatra"));
-//}
-//  // bool chequeoNacimiento(tm fecha) 
+TEST(chequeo_direccion,direc_simbolos)
+{
+	ASSERT_FALSE(chequeo_direccion("centr!o a(sturiano"));
+}
+TEST(chequeo_direccion, direc_may)
+{
+	ASSERT_TRUE(chequeo_direccion("Centro Asturiano"));
+}
+TEST(chequeo_direccion, direc_puntos)
+{
+	ASSERT_FALSE(chequeo_direccion("centro asturian.o"));
+}
+TEST(chequeo_direccion, direc_comun1)
+{	ASSERT_TRUE(chequeo_direccion("centro asturiano"));
+}
+TEST(chequeo_direccion, direc_comun2)
+{
+	ASSERT_TRUE(chequeo_direccion("centro asturiano 1234"));
+}
+TEST(chequeo_celular, cel_simbolos)
+{
+	ASSERT_FALSE(chequeo_celular("2*$,55"));
+}
+TEST(chequeo_celular, cel_letras)
+{
+	ASSERT_FALSE(chequeo_celular("2abbbbc"));
+}
+TEST(chequeo_celular, cel_comun)
+{
+	ASSERT_TRUE(chequeo_celular("+54(801)04193744"));
+}
+TEST(chequearfecha, fecha_negativa )
+{
+	ASSERT_FALSE(chequearfecha(-2, -6, -2020));
+}
+TEST(chequearfecha, fecha_larga)
+{
+	ASSERT_FALSE(chequearfecha(222, 500, 22020));
+}
+TEST(chequearfecha, fecha_mesmal)
+{
+	ASSERT_FALSE(chequearfecha(2, 16, 2020));
+}
+TEST(chequearfecha, fecha_comun)
+{
+	ASSERT_TRUE(chequearfecha(15, 11, 2022));
+}
+TEST(chequeo_especialidades,especialidad_numero )
+{
+	ASSERT_FALSE(chequeo_especialidades("55555"));
+}
+TEST(chequeo_especialidades, especialidad_simbolos)
+{
+	ASSERT_FALSE(chequeo_especialidades("¿?-¨´"));
+}
+TEST(chequeo_especialidades, especialidad_mayuscula)
+{
+	ASSERT_TRUE(chequeo_especialidades("medico_Clinico"));
+}
+TEST(chequeo_especialidades, especialidad_minuscula)
+{
+	ASSERT_TRUE(chequeo_especialidades("medico_pediatra"));
+}
+ // bool chequeoNacimiento(tm fecha) 
 //TEST(chequeoNacimiento, fecha_negativa)
 //{
 //	tm fecha; // creamos un estructura fecha
@@ -271,31 +264,27 @@
 //	ASSERT_FALSE(chequeoNacimiento(fecha));
 //}
 // //bool chequeo_estado(string estado);
-//
-//TEST(chequeo_estado, estado_incorrecto ) 
-//{
-//	
-//	ASSERT_FALSE(chequeo_estado("de_alta"));
-//}
-//TEST(chequeo_estado, estado_con_simbolo)
-//{
-//
-//	ASSERT_FALSE(chequeo_estado("@ "));
-//}
-//TEST(chequeo_estado, estado_CORRECTO)
-//{
-//
-//	ASSERT_TRUE(chequeo_estado("fallecido"));
-//}
-//TEST(chequeo_estado, estado_MAYUSCULA)
-//{
-//
-//	ASSERT_FALSE(chequeo_estado("FALLECIDO"));
-//}
-// //bool chequeofechasolicitado(tm fecha_solicitado, tm fecha_turno);
+TEST(chequeo_estado, estado_incorrecto ) 
+{	
+	ASSERT_FALSE(chequeo_estado("de_alta"));
+}
+TEST(chequeo_estado, estado_con_simbolo)
+{
+	ASSERT_FALSE(chequeo_estado("@ "));
+}
+TEST(chequeo_estado, estado_CORRECTO)
+{
+ASSERT_TRUE(chequeo_estado("fallecido"));
+}
+TEST(chequeo_estado, estado_MAYUSCULA)
+{
+
+ASSERT_FALSE(chequeo_estado("FALLECIDO"));
+}
+//bool chequeofechasolicitado(tm fecha_solicitado, tm fecha_turno);
 //TEST(chequeofechasolicitado, fecha_solicitado_mayor_a_fecha_turno)
 //{
-//
+
 //	tm fecha_s; // fecha solicitado
 //	fecha_s.tm_year = 2020;
 //	fecha_s.tm_mon = 11;
@@ -358,13 +347,9 @@
 //
 //	ASSERT_FALSE(chequeofechasolicitado(fecha_s, fecha_t)); // chequear si las fechas al ser iguales tiene que dar verdadero o falso
 //}
-// // bool chequeoObra_social(string Obra_soc, Obra_Social* listaObra_soc, int tam)
-//
-//
-//
-//
+
 ////bool agregar_paciente(Paciente aux, Paciente*& lista, int* tam);
-//
+
 //TEST(agregar_paciente, agregar_paciente_correcto)
 //{
 //	Paciente aux;
@@ -511,52 +496,50 @@
 //
 //
 //
-//TEST(buscartelefono, buscartelefono_correcto)
-//{
-//
-//	Contactos aux1;
-//	Contactos aux2;
-//	Contactos* lista = new Contactos[2];
-//	aux1.Celular = "+41 203 339 0504";
-//	aux1.Direccion ="callefalse 123";
-//	aux1.DNI_paciente= "67597385";
-//	aux1.Mail ="pepito@hotmail.com";
-//	aux1.Telefono = "+57 722 907 7201";
-//	aux2.Celular = "+40 203 339 0504";
-//	aux2.Direccion = "callefalse 123";
-//	aux2.DNI_paciente = "69597385";
-//	aux2.Mail = "pepito@hotmail.com";
-//	aux2.Telefono = "+57 725 906 7201";
-//	lista[0] = aux1;
-//	lista[1] = aux2;
-//	int tam = 2;
-//
-//	ASSERT_EQ(buscartelefono(aux1.DNI_paciente, lista, tam),"+57 722 907 7201");
-//	delete[] lista;
-//}
-//TEST(buscartelefono, buscartelefono_incorrecto)
-//{
-//
-//	Contactos aux1;
-//	Contactos aux2;
-//	Contactos* lista = new Contactos[2];
-//	aux1.Celular = "+41 203 339 0504";
-//	aux1.Direccion = "callefalse 123";
-//	aux1.DNI_paciente = "67597385";
-//	aux1.Mail = "pepito@hotmail.com";
-//	aux1.Telefono = "+57 722 907 7201";
-//	aux2.Celular = "+40 203 339 0504";
-//	aux2.Direccion = "callefalse 123";
-//	aux2.DNI_paciente = "69597385";
-//	aux2.Mail = "pepito@hotmail.com";
-//	aux2.Telefono = "+57 725 906 7201";
-//	lista[0] = aux1;
-//	lista[1] = aux2;
-//	int tam = 2;
-//
-//	ASSERT_NE(buscartelefono(aux1.DNI_paciente, lista, tam), "+58 721 907 7201");
-//	delete[] lista;
-//}
+TEST(buscartelefono, buscartelefono_correcto)
+{
+
+	Contactos aux1;	Contactos aux2;
+	Contactos* lista = new Contactos[2];
+	aux1.Celular = "+54(801)04193744";
+	aux1.Direccion ="callefalse 123";
+	aux1.DNI_paciente= "67597385";
+	aux1.Mail ="pepito@hotmail.com";
+	aux1.Telefono = "+54(426)84659920";
+	aux2.Celular = "+54(316)12652502";
+	aux2.Direccion = "calle 125";
+	aux2.DNI_paciente = "69597385";
+	aux2.Mail = "pepito@hotmail.com";
+	aux2.Telefono = "+54(176)68420326";
+	lista[0] = aux1;
+	lista[1] = aux2;
+	int tam = 2;
+	ASSERT_EQ(buscartelefono(aux1.DNI_paciente, lista, tam),"+54(426)84659920");
+	delete[] lista;
+}
+TEST(buscartelefono, buscartelefono_incorrecto)
+{
+
+	Contactos aux1;
+	Contactos aux2;
+	Contactos* lista = new Contactos[2];	
+	aux1.Celular = "+54(801)04193744";
+	aux1.Direccion = "callefalse 123";
+	aux1.DNI_paciente = "67597385";
+	aux1.Mail = "pepito@hotmail.com";
+	aux1.Telefono = "+54(426)84659920";
+	aux2.Celular = "+54(316)12652502";
+	aux2.Direccion = "callefalse 123";
+	aux2.DNI_paciente = "69597385";
+	aux2.Mail = "pepito@hotmail.com";
+	aux2.Telefono = "+54(176)68420326";
+	lista[0] = aux1;
+	lista[1] = aux2;
+	int tam = 2;
+
+	ASSERT_NE(buscartelefono(aux1.DNI_paciente, lista, tam), "+54(176)68420326");
+	delete[] lista;
+}
 //// bool agregar_medico(Medicos aux, Medicos*& lista, int* tam);
 //TEST(agregar_medico, agregar_medico_incorrecto)
 //{
